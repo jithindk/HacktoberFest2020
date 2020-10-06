26c0eda299be9600da522ad529c4d817a6f95b8d
Jithin D K <43891353+jithindk@users.noreply.github.com>
2020-10-06 18:32:45 +0530

    Update Factorial_number.py

--- Basic Programs/Factorial_number.py
+++ Basic Programs/Factorial_number.py
@@ -1,12 +1,10 @@
 # Python 3 program to find  
-# factorial of given number using recursive
+# factorial of a number using recursive
 def factorial(n): 
       
     # single line to find factorial 
-    return 1 if (n==1 or n==0) else n * factorial(n - 1);  
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
   
 # Driver Code 
-num = 5; 
-print("Factorial of",num,"is", 
-factorial(num)) 
-  
+num = int(input('Enter number : '))
+print("Factorial of",num,"is", factorial(num)) 
